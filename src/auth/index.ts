02ccc024b2dccd58ff0a44a222986d4b1836b532
Avi <123avi@gmail.com>
2026-04-06 08:49:13 +0400

    feat: task 1 - Setup project structure and package.json
    Closes #542

---
+++ src/auth/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Authentication service barrel export
+ * Provides centralized access to auth-related functionality
+ */
+
+// Placeholder for auth service exports
+// This will be implemented in subsequent tasks
+export {};
